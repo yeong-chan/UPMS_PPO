e75c5fec5fbdee188d0281fc1b5962fb7552ef77
yeong-chan <125521626+yeong-chan@users.noreply.github.com>
2023-07-28 17:02:31 +0900

    Update _test_ppo.py

--- _test_ppo.py
+++ _test_ppo.py
@@ -1,7 +1,9 @@
 from cfg import get_cfg
 import os
 import pandas as pd
+#from dqn import *
 import random
+
 from agent.ppo import *
 from environment.env import UPMSP
 
@@ -12,13 +14,17 @@ if __name__ == "__main__":
     torch.manual_seed(42)
     np.random.seed(42)
     random.seed(42)
-    num_episode = 10000
+    num_episode = 30
     episode = 1
     score_avg = 0
     state_size =104
-    action_size = 12
 
     mode = cfg.mode
+    if mode == 'heuristic':
+        action_size = 3
+    else:
+        action_size = 12
+
     lr = cfg.lr
     gamma = cfg.gamma
     lmbda = cfg.lmbda
@@ -30,19 +36,30 @@ if __name__ == "__main__":
     w_move = cfg.w_move
     w_priority = cfg.w_priority
     state_size = 104        # feature 1~8 size = 104 / 176
-    # log_path = '../result/model/dqn'
-    # if not os.path.exists(log_path):
-    #     os.makedirs(log_path)
+    log_path = '../result/model/dqn'
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
 
     event_path = '../environment/result/dqn'
     if not os.path.exists(event_path):
         os.makedirs(event_path)
 
-    env = UPMSP(log_dir=event_path, num_j=cfg.n_job, num_m=8, action_number=action_size, min=0.1, max=4, action_mode=mode)  # action_mode 바꿔야 함 heuristic, WCOVERT
+    #env = UPMSP(log_dir=event_path, num_j=1500,num_m=8, action_number = action_size, action_mode = 'WCOVERT')
+    env = UPMSP(log_dir=event_path, num_j=800, num_m=8, action_number=action_size, min=0.1, max=4, action_mode=mode)  # action_mode 바꿔야 함 heuristic, WCOVERT
     agent = Agent(state_size, action_size, lr, gamma, lmbda, eps_clip, K_epoch)
-    agent.network.load_state_dict(torch.load('/root/UPMS_PPO/episode4800_atc_ppo12.pt')["model_state_dict"])
+    #agent.network.load_state_dict(torch.load('../result/model/dqn/episode8000.pt')["model_state_dict"])    # WCOVERT PPO
+    # agent.network.load_state_dict(torch.load('../result/model/ppo/episode6200.pt')["model_state_dict"])   # heuristic PPO
+    # agent.network.load_state_dict(torch.load('../result/model/ppo/episode4800_atc_ppo12.pt')["model_state_dict"])     # ATC PPO
+    #agent.network.load_state_dict(torch.load('../result/model/ppo/episode200_heuristic_ppo_3.pt')["model_state_dict"])     # heuristic PPO
+
+
+
+    # q = Qnet(state_size, action_size)
+    # q.load_state_dict(torch.load(log_path + '/episode5000.pt')["model_state_dict"])       # 가중치가 저장되는 경로
 
     tard_list = list()
+    np.random.seed(42)
+    random.seed(42)
     #print(num_episode)
     for i in range(num_episode):
         env.e = i
@@ -69,7 +86,7 @@ if __name__ == "__main__":
 
         mean_wt = env.monitor.tardiness / env.num_job
         tard_list.append(mean_wt)
-        #print("Episode {0} | MWT = {1} | CUM_MWT = {2}".format(i+1, mean_wt, np.mean(tard_list)))
         print("{}".format(-mean_wt))
+        # print("Episode {0} | MWT = {1} | CUM_MWT = {2}".format(i+1, -mean_wt, -np.mean(tard_list)))
 
     print("Total Mean Weighted Tardiness = ", np.mean(tard_list))
